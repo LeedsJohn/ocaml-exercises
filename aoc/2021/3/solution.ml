137130a0a1518a6db8190ee82e6ec808c8e89a7d
John Leeds <leedsjohn6@gmail.com>
2023-03-23 14:41:25 -0500

    finish part 1

--- aoc/2021/3/solution.ml
+++ aoc/2021/3/solution.ml
@@ -9,20 +9,22 @@ let numbers fname =
     res
 
 let bin_to_dec bin =
-    let rec aux power i = if i = Array.length bin then 0 else bin.(i) * power + aux (power * 2) (i + 1)
+    let rec aux power i = if i = -1 then 0 else bin.(i) * power + aux (power * 2) (i - 1)
     in
-    aux 1 0
+    aux 1 ((Array.length bin) - 1)
 
 let invert bin = Array.map bin ~f:(fun x -> abs (x - 1))
 
 let part1 nums =
     let length = Array.length (List.hd_exn nums) in
+    let goal = (List.length nums) / 2 in
     let res = Array.create ~len:length 0 in (* number of 1 bits in each position *)
-    let process_num ar = for i = 0 to (length - 1) do res.(i) <- res.(i) + ar.(i) done in 
-    List.iter nums ~f:process_num;
-    for i = 0 to length - 1 do Stdio.print_string (Int.to_string res.(i)) done;
+    let get_num_ones ar = for i = 0 to (length - 1) do res.(i) <- res.(i) + ar.(i) done in 
+    List.iter nums ~f:get_num_ones;
+    let get_bit n = if n > goal then 1 else 0 in
+    let res = Array.map res ~f:get_bit in
     (bin_to_dec res) * (bin_to_dec (invert res))
 
 
-let () = Stdio.print_endline ("Part 1: " ^ (Int.to_string (part1 (numbers "input2.txt"))))
+let () = Stdio.print_endline ("Part 1: " ^ (Int.to_string (part1 (numbers "input.txt"))))
 (* let () = Stdio.print_endline ("Part 2: " ^ (Int.to_string (part2 commands))) *)
