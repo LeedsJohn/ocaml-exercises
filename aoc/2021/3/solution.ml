84bb5aff4391b003407099715e81c4b8b6461862
John Leeds <leedsjohn6@gmail.com>
2023-05-24 14:07:16 -0500

    day 3 part 2

--- aoc/2021/3/solution.ml
+++ aoc/2021/3/solution.ml
@@ -1,5 +1,76 @@
 open Base
 
+let fname = "input.txt"
+;;
+
+let numbers = 
+    let file = Stdio.In_channel.create fname in
+    let get_bits line = Array.map ~f:(fun c -> String.of_char c |> Int.of_string) (String.to_array line)
+    in
+    let res = List.map ~f:get_bits (Stdio.In_channel.input_lines file) in
+    In_channel.close file;
+    res
+;;
+
+let m = Array.length (List.hd_exn numbers)
+;;
+
+let most_common_bit nums i  =
+    let sum = List.fold
+        (List.map nums ~f:(fun ar -> ar.(i)))
+        ~f:(+) ~init:0
+    in
+    if sum * 2 >= (List.length nums) then 1 else 0
+;;
+
+let least_common_bit nums i = if (most_common_bit nums i) = 1 then 0 else 1
+;;
+
+let bin_to_dec bin =
+    List.fold
+        (List.mapi bin ~f:(fun i n -> n * (Int.pow 2 (m - 1 - i))))
+        ~f:(+) ~init:0
+;;
+
+let gamma = List.map (List.range ~stride:1 0 m) ~f:(most_common_bit numbers)
+;;
+
+let epsilon = List.map gamma ~f:(fun n -> if n = 0 then 1 else 0)
+;;
+
+let part1 = (bin_to_dec gamma) * (bin_to_dec epsilon)
+
+let eliminate get_goal_bit i nums =
+    let goal_bit = get_goal_bit nums i
+    in
+    List.filter nums ~f:(fun ar -> ar.(i) = goal_bit)
+;;
+
+let rating eliminate_func =
+    let rec aux i nums =
+        let filtered = eliminate_func i nums in
+        match filtered with
+        | [h] -> Array.to_list h
+        | _   -> aux (i + 1) filtered
+    in
+    aux 0 numbers
+;;
+
+let oxygen = rating (eliminate most_common_bit)
+;;
+
+let carbon_dioxide = rating (eliminate least_common_bit)
+;;
+
+let part2 = (bin_to_dec oxygen) * (bin_to_dec carbon_dioxide)
+
+let () = Stdio.printf "\nPart 1: %d\n" part1
+
+let () = Stdio.printf "\nPart 2: %d\n" part2
+
+
+(* open Base
+
 let numbers fname = 
     let file = Stdio.In_channel.create fname in
     let get_bits line = Array.map ~f:(fun c -> String.of_char c |> Int.of_string) (String.to_array line)
@@ -27,4 +98,4 @@ let part1 nums =
 
 
 let () = Stdio.print_endline ("Part 1: " ^ (Int.to_string (part1 (numbers "input.txt"))))
-(* let () = Stdio.print_endline ("Part 2: " ^ (Int.to_string (part2 commands))) *)
+let () = Stdio.print_endline ("Part 2: " ^ (Int.to_string (part2 commands))) *)
